e0b76a9a4f960eda2f3b584d349b828d5525caf7
mu-files <info@mu-files.com>
2026-07-10 10:28:41 -0700

    Add custom astropy hook and Pillow for PyInstaller builds

---
+++ mu-dng-converter/hook-astropy.py
@@ -0,0 +1,11 @@
+# Override the default astropy hook to avoid collecting visualization modules
+# that require matplotlib. We only need astropy.io.fits and astropy.coordinates.
+
+# Don't collect all submodules - only specify what we actually need
+hiddenimports = [
+    'astropy.io.fits',
+    'astropy.io.fits.hdu',
+    'astropy.io.fits.header',
+    'astropy.coordinates',
+    'astropy.units',
+]
