b462da1eb7725d4d01d60b0588c32d16e63f67fa
mu-files <info@mu-files.com>
2026-07-10 16:31:42 -0700

    Add local CI sandbox and fix astropy.constants.iau2015 import

---
+++ mu-dng-converter/hook-astropy.py
@@ -0,0 +1,25 @@
+# Override the default astropy hook to avoid collecting visualization modules
+# that require matplotlib. We only need astropy.io.fits and astropy.coordinates.
+
+from PyInstaller.utils.hooks import collect_data_files
+
+# Don't collect all submodules - only specify what we actually need
+hiddenimports = [
+    'astropy.io.fits',
+    'astropy.io.fits.hdu',
+    'astropy.io.fits.header',
+    'astropy.coordinates',
+    'astropy.units',
+    'astropy.constants',
+    'astropy.constants.codata2022',
+    'astropy.constants.iau2015',
+]
+
+# Explicitly exclude visualization modules that require matplotlib
+excludedimports = [
+    'astropy.visualization',
+    'matplotlib',
+]
+
+# Include astropy data files (CITATION, etc.)
+datas = collect_data_files('astropy', include_py_files=False)
